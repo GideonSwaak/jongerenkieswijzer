067d7bdd37ccff57fff9f0a566a294a085fdb3c9
KiloulB <kiloulb@gmail.com>
2023-10-16 16:20:02 +0200

    memeimgupdate

--- assets/js/index-v3.js
+++ assets/js/index-v3.js
@@ -11,38 +11,32 @@ mobileToQuestions.addEventListener("click", () => {
   window.location.href = "cards.html";
 });
 
-// Function to open the modal
 function openModal() {
   modal.style.display = "block";
   setTimeout(() => {
     modal.style.height = isMobile() ? "17vh" : "23vh";
-    document.querySelector("footer").style.zIndex = "-1"; // Set z-index to -1 for the footer
+    document.querySelector("footer").style.zIndex = "-1";
   }, 10);
 }
 
-// Function to close the modal
 function closeModal() {
   modal.style.height = "0";
   setTimeout(() => {
     modal.style.display = "none";
-    document.querySelector("footer").style.zIndex = "1"; // Set z-index back to 1 for the footer
+    document.querySelector("footer").style.zIndex = "1";
   }, 300);
 }
 
-// Event listener for opening the modal
 openModalButton.addEventListener("click", openModal);
 
-// Event listener for closing the modal
 closeModalButton.addEventListener("click", closeModal);
 
-// Close the modal if the user clicks outside of it
 window.addEventListener("click", (event) => {
   if (event.target === modal) {
     closeModal();
   }
 });
 
-// Function to check if it's a mobile device
 function isMobile() {
   return window.innerWidth <= 768;
 }
